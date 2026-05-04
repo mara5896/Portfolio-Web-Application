957d01b696a41320a5a6b553f69074209fa50f2b
Mara <mara07476@gmail.com>
2026-05-03 23:09:22 -0400

    Initial Commit

---
+++ src/pages/Chat.js
@@ -0,0 +1,12 @@
+import Chat from "../components/Chat"; 
+
+export default function Chat() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>Feel free to get in touch!</p>
+      <Form />
+       <BlogCard />
+    </div>
+  );
+}
